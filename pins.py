4fa24bf1ebcd484662d2a3a6fb74d4918f58f626
avcourt <andrew.vcourt@gmail.com>
2019-09-04 13:03:14 -0500

    pins.py refactored

--- pins.py
+++ pins.py
@@ -12,21 +12,27 @@ pins = [{'pin_num': 23, 'color': 'red'},
 
 GPIO.setmode(GPIO.BCM) # use GPIO numbering, not generic
 
+
 # setup all pins based on above configuration
 for pin in pins:
 	GPIO.setup(pin['pin_num'], GPIO.OUT, initial=GPIO.LOW)
 
 
-def color_on(color: str):
+def toggle_color(color: str, state: str):
 	pin_nums = [pin['pin_num'] for pin in pins if pin['color'] == color]
 	for pin_num in pin_nums:
-		GPIO.output(pin_num, GPIO.HIGH)
+		if state == 'on'
+			GPIO.output(pin_num, GPIO.HIGH)
+		elif state == 'off':
+			GPIO.output(pin_num, GPIO.LOW)
+
+
+def color_on(color: str):
+	toggle_color(color, 'on')
 
 
 def color_off(color: str):
-	pin_nums = [pin['pin_num'] for pin in pins if pin['color'] == color]
-	for pin_num in pin_nums:
-		GPIO.output(pin_num, GPIO.LOW)
+	toggle_color(color, 'off')
 
 
 def pin_on(pin_num: int):
@@ -45,16 +51,4 @@ def all_on():
 def all_off():
     for pin in pins:
         GPIO.output(pin['pin_num'], GPIO.LOW)
-
-
-def switch_all(state: str):
-    if state == "on":
-        all_on()
-    elif state == "off":
-        all_off()
-    else:
-        print("Error: switch_all() expects str 'on' or 'off'")
-
-
-
 
